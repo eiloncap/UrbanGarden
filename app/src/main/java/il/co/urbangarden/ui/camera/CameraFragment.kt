9008fcb5c1db18d8381e07c1e735545455ce53f2
INB <inbal.lavi1@mail.huji.ac.il>
2021-08-15 12:32:23 +0300

    added main activity - bottom nav

---
+++ app/src/main/java/il/co/urbangarden/ui/camera/CameraFragment.kt
@@ -0,0 +1,44 @@
+package il.co.urbangarden.ui.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import il.co.urbangarden.databinding.FragmentCameraBinding
+
+class CameraFragment : Fragment() {
+
+    private lateinit var cameraViewModel: CameraViewModel
+    private var _binding: FragmentCameraBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        cameraViewModel =
+            ViewModelProvider(this).get(CameraViewModel::class.java)
+
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        cameraViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/il/co/urbangarden/ui/forum/ForumFragment.kt
@@ -0,0 +1,44 @@
+package il.co.urbangarden.ui.forum
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import il.co.urbangarden.databinding.FragmentForumBinding
+
+class ForumFragment : Fragment() {
+
+    private lateinit var forumViewModel: ForumViewModel
+    private var _binding: FragmentForumBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        forumViewModel =
+            ViewModelProvider(this).get(ForumViewModel::class.java)
+
+        _binding = FragmentForumBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        forumViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
