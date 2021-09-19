fb30b0c76e54d95d9fb50d0bbd02f9eabd64c770
amitkadosh284 <amit.kadosh@mail.huji.ac.il>
2021-09-19 15:15:02 +0300

    grid my locations

---
+++ app/src/main/java/il/co/urbangarden/ui/location/LocationDataClass.kt
@@ -0,0 +1,7 @@
+package il.co.urbangarden.ui.location
+
+data class LocationDataClass (
+    var title : String,
+    var image : Int
+    )
+
--- app/src/main/java/il/co/urbangarden/ui/location/MyLocations.kt
+++ app/src/main/java/il/co/urbangarden/ui/location/MyLocations.kt
@@ -1,14 +1,23 @@
 package il.co.urbangarden.ui.location
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import il.co.urbangarden.R
 
+
 class MyLocations : Fragment() {
+    private var dataList = mutableListOf<LocationDataClass>()
+    private var recyclerView: RecyclerView? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var locationAdapter: RecyclerView.Adapter<LocationAdapter.ViewHolder>? = null
+
 
     companion object {
         fun newInstance() = MyLocations()
@@ -20,13 +29,26 @@ class MyLocations : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Inflate the layout for this fragment
+
+        // Add the following lines to create RecyclerView
+
         return inflater.inflate(R.layout.my_locations_fragment, container, false)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MyLocationsViewModel::class.java)
         // TODO: Use the ViewModel
+        //add data
+        recyclerView = view?.findViewById<View>(R.id.recyclerViewMyLocations) as RecyclerView?
+        locationAdapter = LocationAdapter()
+        recyclerView?.layoutManager = GridLayoutManager(context, 2)
+        recyclerView?.adapter = locationAdapter
+        Log.d("recycleView", recyclerView.toString())
+
     }
 
 }
