a50170aef52cc006c679e36de8706f2bdc4db498
אילון <eiloncap@gmail.com>
2021-10-05 10:17:51 +0300

    fixed some shit

--- app/src/main/java/il/co/urbangarden/ui/plants/PlantInfo.kt
+++ app/src/main/java/il/co/urbangarden/ui/plants/PlantInfo.kt
@@ -6,12 +6,9 @@ import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.provider.MediaStore
-import android.text.Editable
-import android.text.TextWatcher
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,16 +24,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.Timestamp
 import il.co.urbangarden.R
 import il.co.urbangarden.data.FirebaseViewableObject
 import il.co.urbangarden.data.location.Location
 import il.co.urbangarden.data.plant.Plant
+import il.co.urbangarden.ml.Model
 import il.co.urbangarden.ui.MainViewModel
 import il.co.urbangarden.ui.home.HomeViewModel
 import il.co.urbangarden.ui.location.LocationAdapter
 import il.co.urbangarden.ui.location.LocationInfo
 import il.co.urbangarden.utils.ImageCropOption
+import org.tensorflow.lite.support.image.TensorImage
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -71,7 +69,6 @@ class PlantInfo : Fragment() {
     private lateinit var delete: ImageView
     private lateinit var locationImg: ImageView
     private lateinit var locationName: TextView
-
 
 
     override fun onCreateView(
@@ -147,7 +144,7 @@ class PlantInfo : Fragment() {
                 mainViewModel.getPlant(res[0].label).let {
                     Log.d("eilon-re", "gor plant $it")
                     if (it != null) {
-                       classifiedPlantDialog(it).show()
+                        classifiedPlantDialog(it).show()
                     }
                 }
 
@@ -162,13 +159,11 @@ class PlantInfo : Fragment() {
 
 
         saveButton.setOnClickListener {
-            if (nameEdit.text.toString().isEmpty()){
+            if (nameEdit.text.toString().isEmpty()) {
                 Toast.makeText(context, "Please Enter Plant Name", Toast.LENGTH_SHORT).show()
-            }
-            else if (inputDays.text.toString().toInt() == 0){
+            } else if (inputDays.text.toString().toInt() == 0) {
                 Toast.makeText(context, "Please enter ", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            } else {
                 plantsViewModel.plant.wateringDays = inputDays.text.toString().toInt()
                 plantsViewModel.plant.name = nameEdit.text.toString()
                 plantsViewModel.plant.notes = notesEdit.text.toString()
@@ -183,7 +178,7 @@ class PlantInfo : Fragment() {
             view.findNavController().navigate(R.id.action_plantInfo_to_navigation_home)
         }
 
-        imgView.setOnClickListener{
+        imgView.setOnClickListener {
             val imgFileName: String = plantsViewModel.plant.uid + ".jpeg"
             plantsViewModel.plant.imgFileName = imgFileName
 
@@ -207,33 +202,33 @@ class PlantInfo : Fragment() {
         //todo share button on click
     }
 
-    private fun setViews(){
+    private fun setViews() {
         val dateFormat: DateFormat = SimpleDateFormat("dd-MM-yy  hh:mm")
         lastStamp.text = dateFormat.format(plantsViewModel.plant.lastWatered).toString()
 
 
-        if (plantsViewModel.plant.locationUid.isNotEmpty()){
+        if (plantsViewModel.plant.locationUid.isNotEmpty()) {
             val location: Location? = mainViewModel.getLocation(plantsViewModel.plant.locationUid)
-            location.let{ mainViewModel.setImgFromPath(location!!, locationImg)
-            locationName.text = location.name
+            location.let {
+                mainViewModel.setImgFromPath(location!!, locationImg)
+                locationName.text = location.name
             }
         }
 
-        if(plantsViewModel.plant.imgFileName.isNotEmpty()){
+        if (plantsViewModel.plant.imgFileName.isNotEmpty()) {
             mainViewModel.setImgFromPath(plantsViewModel.plant, imgView, ImageCropOption.SQUARE)
         }
 
 
-        if (plantsViewModel.plant.name.isEmpty()){
+        if (plantsViewModel.plant.name.isEmpty()) {
             editMode()
-        }
-        else {
+        } else {
             showMode()
 
         }
     }
 
-    private fun editMode(){
+    private fun editMode() {
         nameText.visibility = View.GONE
         speciesText.visibility = View.GONE
         notesText.visibility = View.GONE
@@ -261,7 +256,7 @@ class PlantInfo : Fragment() {
 
     }
 
-    private fun showMode(){
+    private fun showMode() {
         nameText.visibility = View.VISIBLE
         speciesText.visibility = View.VISIBLE
         notesText.visibility = View.VISIBLE
